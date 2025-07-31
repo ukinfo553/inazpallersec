1224e0c93e9381809f49fbf88362904e2bec9bed
vaibhavinfosectrain08 <vaibhav.vats@infosectrain.com>
2025-07-31 15:39:07 +0530

    feat: combo A+ N+

---
+++ lp/comptia-a-plus-network-plus-certification-training/thank-you.php
@@ -0,0 +1,62 @@
+<!--<[thankyou header start]-->
+    <?php include '../thankyou/thankyou-header.php'?>
+<!--<[thankyou header end]-->
+
+<!--/ thankyou courses start /-->
+    <?php
+        $customHeading = 'Are you looking for more <strong>Courses & Certifications?</strong>';
+        include('../lp-components/related-courses.php');
+    ?>
+<!--/ thankyou courses end /-->
+
+<!--/ thankyou blog start /-->
+<section class="thankyou-courses thankyou-sec">
+        <div class="container">
+            <!--/ heading star /-->
+            <div class="row">
+                <div class="col-12 thankyou-h2">
+                    Are you looking for more <span>Blogs?</span>
+                </div>
+            </div>
+            <!--/ heading end /-->
+            <div class="row">
+                <!--/card start /-->
+                <div class="col-lg-3 col-6">
+                    <div class="thankyou--card-item">
+                        <h4>CISM: Frequently Asked questions (FAQs)</h4>
+                        <a href="https://www.infosectrain.com/blog/cism-frequently-asked-questions-faqs/" target="_blank">CHECK NOW</a>
+                    </div>
+                </div>
+                <!--/card end /-->
+                <!--/card start /-->
+                <div class="col-lg-3 col-6">
+                    <div class="thankyou--card-item">
+                        <h4>Tips To Prepare For CISM Exam To Pass It In The First Attempt</h4>
+                        <a href="https://www.infosectrain.com/blog/tips-to-prepare-for-cism-exam-to-pass-it-in-the-first-attempt/" target="_blank">CHECK NOW</a>
+                    </div>
+                </div>
+                <!--/card end /-->
+                <!--/card start /-->
+                <div class="col-lg-3 col-6">
+                    <div class="thankyou--card-item">
+                        <h4>Top Interview Questions for Information Security Manager </h4>
+                        <a href="https://www.infosectrain.com/blog/top-interview-questions-for-information-security-manager/" target="_blank">CHECK NOW</a>
+                    </div>
+                </div>
+                <!--/card end /-->
+                <!--/card start /-->
+                <div class="col-lg-3 col-6">
+                    <div class="thankyou--card-item">
+                        <h4>Cybersecurity Manager Interview Questions</h4>
+                        <a href="https://www.infosectrain.com/blog/cybersecurity-manager-interview-questions/" target="_blank">CHECK NOW</a>
+                    </div>
+                </div>
+                <!--/card end /-->
+            </div>
+        </div>
+    </section>
+<!--/ thankyou blog end /-->
+
+<!--<[thankyou header start]-->
+<?php include '../thankyou/thankyou-footer.php'?>
+<!--<[thankyou header end]-->
