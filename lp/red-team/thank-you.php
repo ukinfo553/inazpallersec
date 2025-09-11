1cf2790b36345c9a2939e7ccc8f8b73b2e7fe84c
njutsav <kutsav817@gmail.com>
2025-09-11 11:47:26 +0530

    Saving my local changes before pull

--- lp/red-team/thank-you.php
+++ lp/red-team/thank-you.php
@@ -1,63 +1,66 @@
-
 <!--<[thankyou header start]-->
-    <?php include '../thankyou/thankyou-header.php'?>
+<?php include '../thankyou/thankyou-header.php'?>
 <!--<[thankyou header end]-->
 
 <!--/ thankyou courses start /-->
-    <?php
+<?php
         $customHeading = 'Are you looking for more <strong>Courses & Certifications?</strong>';
         include('../lp-components/related-courses.php');
     ?>
-    <!--/ thankyou courses end /-->
+<!--/ thankyou courses end /-->
 
-    <!--/ thankyou blog start /-->
-    <section class="thankyou-courses thankyou-sec">
-        <div class="container">
-            <!--/ heading star /-->
-            <div class="row">
-                <div class="col-12 thankyou-h2">
-                    Are you looking for more <span>Blogs?</span>
-                </div>
-            </div>
-            <!--/ heading end /-->
-            <div class="row">
-                <!--/card start /-->
-                <div class="col-lg-3 col-6">
-                    <div class="thankyou--card-item">
-                        <h4>ISO 27001 Lead Auditor Interview Questions for 2024</h4>
-                        <a href="https://www.infosectrain.com/blog/iso-27001-lead-auditor-interview-questions/" target="_blank">CHECK NOW</a>
-                    </div>
-                </div>
-                <!--/card end /-->
-                <!--/card start /-->
-                <div class="col-lg-3 col-6">
-                    <div class="thankyou--card-item">
-                        <h4>Benefits of ISO/IEC 27001 Compliance for Organizations</h4>
-                        <a href="https://www.infosectrain.com/blog/benefits-of-iso-iec-27001-compliance-for-organizations/" target="_blank">CHECK NOW</a>
-                    </div>
-                </div>
-                <!--/card end /-->
-                <!--/card start /-->
-                <div class="col-lg-3 col-6">
-                    <div class="thankyou--card-item">
-                        <h4>ISO 27001: Best Practices to Secure Business Information</h4>
-                        <a href="https://www.infosectrain.com/blog/iso-27001-best-practices-to-secure-business-information/" target="_blank">CHECK NOW</a>
-                    </div>
-                </div>
-                <!--/card end /-->
-                <!--/card start /-->
-                <div class="col-lg-3 col-6">
-                    <div class="thankyou--card-item">
-                        <h4>Audit Techniques and Tools for ISO 27001 Lead Auditors</h4>
-                        <a href="https://www.infosectrain.com/blog/audit-techniques-and-tools-for-iso-27001-lead-auditors/" target="_blank">CHECK NOW</a>
-                    </div>
-                </div>
-                <!--/card end /-->
-
+<!--/ thankyou blog start /-->
+<section class="thankyou-courses thankyou-sec">
+    <div class="container">
+        <!--/ heading star /-->
+        <div class="row">
+            <div class="col-12 thankyou-h2">
+                Are you looking for more <span>Blogs?</span>
             </div>
         </div>
-    </section>
-    <!--/ thankyou blog end /-->
+        <!--/ heading end /-->
+        <div class="row">
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <h4>Interview Questions for Red Team Expert</h4>
+                    <a href="https://www.infosectrain.com/blog/interview-questions-for-red-team-expert/"
+                        target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <h4>Blue Team Vs Red Team: Which One to Choose?</h4>
+                    <a href="https://www.infosectrain.com/blog/blue-team-vs-red-team-which-one-to-choose/"
+                        target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <h4>Penetration Testing vs. Red Teaming</h4>
+                    <a href="https://www.infosectrain.com/blog/penetration-testing-vs-red-teaming/"
+                        target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <!-- <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <h4>Audit Techniques and Tools for ISO 27001 Lead Auditors</h4>
+                    <a href="https://www.infosectrain.com/blog/audit-techniques-and-tools-for-iso-27001-lead-auditors/"
+                        target="_blank">CHECK NOW</a>
+                </div> -->
+            <!-- </div> -->
+            <!--/card end /-->
+
+        </div>
+    </div>
+</section>
+<!--/ thankyou blog end /-->
 
 <!--<[thankyou header start]-->
 <?php include '../thankyou/thankyou-footer.php'?>
