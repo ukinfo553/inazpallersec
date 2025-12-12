33cf4838c694436da72f413882bd61c53b1c7d3b
vaibhavinfosectrain08 <vaibhav.vats@infosectrain.com>
2025-12-12 11:51:00 +0530

    agentic non tech lp

---
+++ lp/agentic-aI-training-for-non-tech/thank-you.php
@@ -0,0 +1,117 @@
+<!--<[thankyou header start]-->
+<?php include '../thankyou/thankyou-header.php' ?>
+<!--<[thankyou header end]-->
+
+<!--/ thankyou courses start /-->
+<?php
+$customHeading = 'Are you looking for more <strong>Courses & Certifications?</strong>';
+include('../lp-components/related-courses.php');
+?>
+<!--/ thankyou courses end /-->
+<!--/ thankyou video start /-->
+<section class="thankyou-courses thankyou-sec" style="background: #ffffff;">
+    <div class="container">
+        <!--/ heading star /-->
+        <div class="row">
+            <div class="col-12 thankyou-h2">
+                Are you looking for more videos on <span>YouTube?</span>
+            </div>
+        </div>
+        <!--/ heading end /-->
+        <div class="row">
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <img src="assets/images/you-tube1.jpg" alt="">
+                    <h4>Complete AI Governance Training</h4>
+                    <a href="https://www.youtube.com/watch?v=zCBqjEMORHY" target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <img src="assets/images/you-tube2.jpg" alt="">
+                    <h4>Boost Productivity with AI in Spreadsheets</h4>
+                    <a href="https://www.youtube.com/watch?v=KbkF8DiWIkw" target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <img src="assets/images/you-tube3.jpg" alt="">
+                    <h4>Future-Ready AI-Powered Cloud Security: Top Tools & Trends in 2026</h4>
+                    <a href="https://www.youtube.com/watch?v=NW06L0E6-Uk&feature=youtu.be" target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <img src="assets/images/you-tube4.jpg" alt="">
+                    <h4>How AI is Transforming Cybersecurity | Benefits, Risks & Controls</h4>
+                    <a href="https://www.youtube.com/watch?v=uDNovwMSgu0&feature=youtu.be" target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+        </div>
+    </div>
+</section>
+<!--/ thankyou video end /-->
+<!--/ thankyou blog start /-->
+<section class="thankyou-courses thankyou-sec">
+    <div class="container">
+        <!--/ heading star /-->
+        <div class="row">
+            <div class="col-12 thankyou-h2">
+                Are you looking for more <span>Blogs?</span>
+            </div>
+        </div>
+        <!--/ heading end /-->
+        <div class="row">
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <h4>Centralized vs. Federated vs. Decentralized AI Governance</h4>
+                    <a href="https://www.infosectrain.com/blog/centralized-vs-federated-vs-decentralized-ai-governance/"
+                        target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <h4>Types of Artificial Intelligence (AI) Systems</h4>
+                    <a href="https://www.infosectrain.com/blog/types-of-artificial-intelligence-ai-systems/"
+                        target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <h4>AI Auditing Tools and Techniques</h4>
+                    <a href="https://www.infosectrain.com/blog/ai-auditing-tools-and-techniques/" target="_blank">CHECK
+                        NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+            <!--/card start /-->
+            <div class="col-lg-3 col-6">
+                <div class="thankyou--card-item">
+                    <h4>AI Associated Risks and How to Mitigate Them?</h4>
+                    <a href="https://www.infosectrain.com/blog/ai-associated-risks-and-how-to-mitigate-them/"
+                        target="_blank">CHECK NOW</a>
+                </div>
+            </div>
+            <!--/card end /-->
+
+        </div>
+    </div>
+</section>
+<!--/ thankyou blog end /-->
+
+<!--<[thankyou header start]-->
+<?php include '../thankyou/thankyou-footer.php' ?>
+<!--<[thankyou header end]-->
