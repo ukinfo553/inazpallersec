1db26c9d6a84d9e86fd1e2e2326663b85370e085
vaibhavinfosectrain08 <vaibhav.vats@infosectrain.com>
2026-01-19 10:11:22 +0530

    LP wazuh and ciso

--- lp/wazuh-practical-training/include/comman_use.php
+++ lp/wazuh-practical-training/include/comman_use.php
@@ -2,7 +2,7 @@
 session_start();
 $refferer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : "";
 $requesturl = isset($_SERVER['REQUEST_URI']) ? 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] : "";
-define("BASE_URL", "https://www.infosectrain.com/pages/lp/ciso-practical-training/");
+define("BASE_URL", "https://www.infosectrain.com/pages/lp/wazuh-practical-training");
 //define("BASE_URL", "http://localhost/infosectrain.com/pages/lp/cissp-training-dubai/");
 define("MINFILE", ".min"); //.min
 
